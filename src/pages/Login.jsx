296ad68f58911266ee2fdf26d6ae2f361a8db4c8
rafael <rafaelsm003@gmail.com>
2022-06-20 17:42:03 -0300

    feat: adiciona a página de login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Input from '../components/Input';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const enabelButton = () => {
+    const MIN_CHAR = 6;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email) && password.length > MIN_CHAR;
+  };
+
+  return (
+    <form className="login-container">
+      <Input
+        testId="email-input"
+        id="email"
+        type="text"
+        onChange={ setEmail }
+        labelText="E-mail"
+        value={ email }
+      />
+
+      <Input
+        testId="password-input"
+        id="password"
+        type="password"
+        onChange={ setPassword }
+        labelText="Password"
+        value={ password }
+      />
+
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ !enabelButton() }
+      >
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default Login;
