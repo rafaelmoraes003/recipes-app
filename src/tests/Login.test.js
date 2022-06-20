8e5bc3546660bc055f4c5f0c8ba92961e1549933
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-20 20:07:37 -0300

    feat: Inclusão de teste para localStorage

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from './helpers/renderWithRouter';
+import renderWithRouterAndStore from './helpers/renderWithRouterAndStore';
 import App from '../App';
 
 describe('Testa a tela de Login', () => {
   it('Verifica se a URL é a raiz', () => {
-    const { history } = renderWithRouter(<App />);
+    const { history } = renderWithRouterAndStore(<App />);
     const { pathname } = history.location;
     expect(pathname).toBe('/');
   });
 
   it('Verifica a existência dos inputs de email e password e do botão', () => {
-    renderWithRouter(<App />);
+    renderWithRouterAndStore(<App />);
     const emailInput = screen.getByRole('textbox', { name: /e-mail/i });
     const passwordInput = screen.getByLabelText(/password/i);
     const button = screen.getByRole('button', { name: /login/i });
@@ -22,13 +22,13 @@ describe('Testa a tela de Login', () => {
   });
 
   it('Verifica se o botão está desabilitado quando carrega a página', () => {
-    renderWithRouter(<App />);
+    renderWithRouterAndStore(<App />);
     const button = screen.getByRole('button', { name: /login/i });
     expect(button).toBeDisabled();
   });
 
   it('Verifica as condições de habilitação do botão (false)', () => {
-    renderWithRouter(<App />);
+    renderWithRouterAndStore(<App />);
     const emailInput = screen.getByRole('textbox', { name: /e-mail/i });
     const passwordInput = screen.getByLabelText(/password/i);
     const button = screen.getByRole('button', { name: /login/i });
@@ -50,7 +50,7 @@ describe('Testa a tela de Login', () => {
   });
 
   it('Verifica as condições de habilitação do botão (true)', () => {
-    renderWithRouter(<App />);
+    renderWithRouterAndStore(<App />);
     const emailInput = screen.getByRole('textbox', { name: /e-mail/i });
     const passwordInput = screen.getByLabelText(/password/i);
     const button = screen.getByRole('button', { name: /login/i });
@@ -59,5 +59,18 @@ describe('Testa a tela de Login', () => {
     userEvent.type(passwordInput, '1234567');
 
     expect(button).not.toBeDisabled();
+  });
+
+  it('Verifica se os tokens são salvos na localStorage', () => {
+    renderWithRouterAndStore(<App />);
+    const emailInput = screen.getByRole('textbox', { name: /e-mail/i });
+    const passwordInput = screen.getByLabelText(/password/i);
+    const button = screen.getByRole('button', { name: /login/i });
+
+    userEvent.type(emailInput, 'teste1@teste.com');
+    userEvent.type(passwordInput, '1234567');
+    userEvent.click(button);
+
+    expect(localStorage).toHaveProperty('email', '"teste1@teste.com"');
   });
 });
