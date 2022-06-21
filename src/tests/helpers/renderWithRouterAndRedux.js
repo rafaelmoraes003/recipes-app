dcbdbc8275a1a2551d76289d15811ef279776527
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-21 18:33:05 -0300

    fix (renderWithRouterAndRedux): Correção de helper

---
+++ src/tests/helpers/renderWithRouterAndRedux.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
+import rootReducer from '../../redux/reducer';
+
+export const renderWithRouterAndRedux = (component, initialState, route = '/') => {
+  const store = createStore(rootReducer, initialState);
+  const history = createMemoryHistory({ initialEntries: [route] });
+
+  return {
+    ...render(
+      <Provider store={ store }>
+        <Router history={ history }>
+          {component}
+        </Router>
+      </Provider>,
+    ),
+    history,
+    store,
+  };
+};
+
+export default renderWithRouterAndRedux;
