b21c2470faa75008c22a1e59df01c400e30519d2
igor alex <igoralex8701@gmail.com>
2022-06-20 22:35:39 -0300

    Mudança de button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/profile" component={ Profile } />
     </Switch>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
+
+const Header = () => {
+  const history = useHistory();
+  const onClickProfile = () => {
+    console.log('entrei');
+    history.push('/profile');
+  };
+  return (
+    <div>
+
+      <button
+        type="button"
+        data-testid="profile-top-btn"
+        onClick={ onClickProfile }
+      >
+        <img src={ profileIcon } alt="Botão com icon de pergil" />
+      </button>
+      <input data-testid="search-input" />
+
+      <h1 data-testid="page-title">Foods</h1>
+      <button type="button" data-testid="search-top-btn">
+        <img src={ searchIcon } alt="Botão com icon de lupa/pesquisa" />
+      </button>
+    </div>
+  );
+};
+
+Header.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+
+};
+
+export default Header;
