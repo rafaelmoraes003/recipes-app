76e91c1a2ba449374fa782c2b777bf47aa1175dc
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-21 22:28:14 -0300

    feat (Foods.jsx): Inclusão de cards para receitas

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import Header from '../components/Header';
 import { saveInitialFoods } from '../redux/actions';
 import { fetchFoods } from '../helpers/fetchRecipesAPI';
+import RecipeCard from '../components/RecipeCard';
 
 const Foods = () => {
   const totalRecipesNumber = 12;
@@ -22,6 +23,14 @@ const Foods = () => {
   return (
     <div>
       <Header title="Foods" showSearchIcon />
+      {recipesFoods.map(({ idMeal, strMeal, strMealThumb }, index) => (
+        <RecipeCard
+          key={ idMeal }
+          index={ index }
+          foodName={ strMeal }
+          foodImage={ strMealThumb }
+        />
+      ))}
     </div>
   );
 };
