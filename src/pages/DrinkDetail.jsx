3543201827d32c34888b71282e6b867d744c49bd
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-22 20:12:51 -0300

    feat (DrinkDetail): estrutura da página

--- src/pages/DrinkDetail.jsx
+++ src/pages/DrinkDetail.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 const DrinkDetail = () => (
-  <h1>Details Drinks</h1>
+  <section>
+    <img src="123" alt="123" data-testid="recipe-photo" />
+    <h1 data-testid="recipe-title">123</h1>
+    <h3 data-testid="recipe-category">123</h3>
+    <button type="button" data-testid="share-btn">Compartilhar Receita</button>
+    <button type="button" data-testid="favorite-btn">Favoritar Receita</button>
+    {/* TODO: ingredients => data-testid="${index}ingredient-name-and-measure"} */}
+    <p data-testid="instructions">123</p>
+    {/* TODO: receitas recomendadas => data-testid="start-recipe-btn" */}
+  </section>
 );
 export default DrinkDetail;
