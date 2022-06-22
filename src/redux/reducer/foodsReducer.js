6e561c3e751c5d66110c9b9ce14d53c05e225821
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-21 21:29:53 -0300

    feat (foodsReducer): implementação do foodsReducer

---
+++ src/redux/reducer/foodsReducer.js
@@ -0,0 +1,19 @@
+import { SAVE_INITIAL_FOODS } from '../actions';
+
+const INITIAL_STATE = {
+  foods: {},
+};
+
+const foodsReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SAVE_INITIAL_FOODS:
+    return {
+      ...state,
+      foods: action.payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default foodsReducer;
