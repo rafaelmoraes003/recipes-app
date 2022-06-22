c269e9335159537e4b2e031b85298cf97dd536ca
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-22 15:10:15 -0300

    fix (CategoryButton): Inclusão de espaço

--- src/components/CategoryButto.jsx
+++ src/components/CategoryButto.jsx
@@ -5,6 +5,7 @@ function CategoryButton({ categoryName }) {
   return (
     <label htmlFor={ categoryName }>
       <input type="radio" id={ categoryName } name="category" value={ categoryName } />
+      {' '}
       { categoryName }
     </label>
   );
