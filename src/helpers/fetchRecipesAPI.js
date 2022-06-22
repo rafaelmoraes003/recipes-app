5bdaa997a086913001478d71cfec0d8d3497446a
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-21 22:06:20 -0300

    fix (fetchRecipes): Inclusãopara fetch ser preciso

--- src/helpers/fetchRecipesAPI.js
+++ src/helpers/fetchRecipesAPI.js
@@ -1,7 +1,7 @@
 export const fetchFoods = async () => {
   const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
   const dataFoods = await response.json();
-  return dataFoods;
+  return dataFoods.meals;
 };
 
 export const fetchDrinks = async () => {
