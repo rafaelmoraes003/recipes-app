bfc736d67e0dcfc46dbd7e30e239417dc87dea89
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-21 22:10:22 -0300

    feat (Foods.jsx): Lógica para carregar receitas

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Header from '../components/Header';
+import { saveInitialFoods } from '../redux/actions';
+import { fetchFoods } from '../helpers/fetchRecipesAPI';
 
 const Foods = () => {
-  const [recipesFoods, setRecipesFoods] = useState();
+  const totalRecipesNumber = 12;
+  const [recipesFoods, setRecipesFoods] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loadsFoodRecipes = async () => {
+      const recipesData = await fetchFoods();
+      const usableRecipes = recipesData
+        .filter((recipe, index) => index < totalRecipesNumber);
+      setRecipesFoods(usableRecipes);
+      dispatch(saveInitialFoods(usableRecipes));
+    };
+    loadsFoodRecipes();
+  }, [dispatch]);
   return (
     <div>
       <Header title="Foods" showSearchIcon />
