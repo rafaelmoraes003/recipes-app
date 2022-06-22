3cb0e5ebf286c17b43ed800448da22980ad69f3d
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-21 21:31:46 -0300

    feat(reducers/index.js): Inclusão do foodsReducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
+import foodsReducer from './foodsReducer';
 
-const rootReducer = combineReducers({ userReducer });
+const rootReducer = combineReducers({ userReducer, foodsReducer });
 
 export default rootReducer;
