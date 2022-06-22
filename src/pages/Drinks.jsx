816790d81d66c85f7d0dc169937c51fd951958d0
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-21 22:47:55 -0300

    feat (Drinks.jsx): Estrutura para state de drinks

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Header from '../components/Header';
+import { saveInitialDrinks } from '../redux/actions';
+import { fetchDrinks } from '../helpers/fetchRecipesAPI';
+import RecipeCard from '../components/RecipeCard';
 
-const Drinks = () => (
-  <Header title="Drinks" showSearchIcon />
-);
+const Drinks = () => {
+  const totalRecipesNumber = 12;
+  const [recipesDrinks, setRecipesDrinks] = useState([]);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <Header title="Drinks" showSearchIcon />
+
+    </div>
+  );
+};
 export default Drinks;
