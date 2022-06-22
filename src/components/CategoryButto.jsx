a11cc9b86e3eb9c19836c66d9caa8fe76df7be34
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-22 16:59:20 -0300

    perf (CategoryButton): Inclusão d função de filtro

--- src/components/CategoryButto.jsx
+++ src/components/CategoryButto.jsx
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function CategoryButton({ categoryName }) {
+function CategoryButton({ categoryName, searchFunc }) {
   return (
     <label htmlFor={ categoryName }>
-      <input type="radio" id={ categoryName } name="category" value={ categoryName } />
+      <input
+        data-testid={ `${categoryName}-category-filter` }
+        type="radio"
+        id={ categoryName }
+        name="category"
+        value={ categoryName }
+        onClick={ () => searchFunc(categoryName) }
+      />
       {' '}
       { categoryName }
     </label>
@@ -13,6 +20,7 @@ function CategoryButton({ categoryName }) {
 
 CategoryButton.propTypes = {
   categoryName: PropTypes.string.isRequired,
+  searchFunc: PropTypes.func.isRequired,
 };
 
 export default CategoryButton;
