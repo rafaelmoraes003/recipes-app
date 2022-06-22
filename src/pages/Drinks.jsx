71a0dcda5feed2e37efc04c8328d6fa6a585c153
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-22 15:15:57 -0300

    feat (Drinks): Inclusão de buttons para categoria

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -4,16 +4,31 @@ import Header from '../components/Header';
 import { saveInitialDrinks } from '../redux/actions';
 import { fetchDrinks } from '../helpers/fetchRecipesAPI';
 import RecipeCard from '../components/RecipeCard';
+import CategoryButton from '../components/CategoryButto';
 
 const Drinks = () => {
   const totalRecipesNumber = 12;
   const [recipesDrinks, setRecipesDrinks] = useState([]);
+  const [categoryDrinks, setCategoryDrinks] = useState([]);
   const { data } = useSelector((state) => state.apiReducer);
   const dispatch = useDispatch();
+
+  const selectsCategories = (drinksArray) => {
+    const categoriesNumber = 5;
+    const categories = drinksArray.reduce((acc, { strCategory }) => {
+      if (acc.length < categoriesNumber) {
+        if (acc.includes(strCategory)) return acc;
+        return [...acc, strCategory];
+      }
+      return acc;
+    }, []);
+    setCategoryDrinks(categories);
+  };
 
   useEffect(() => {
     const loadsDrinksRecipes = async () => {
       const recipesData = await fetchDrinks();
+      selectsCategories(recipesData);
       const usableRecipes = recipesData
         .filter((recipe, index) => index < totalRecipesNumber);
       setRecipesDrinks(usableRecipes);
@@ -31,6 +46,14 @@ const Drinks = () => {
   return (
     <div>
       <Header title="Drinks" showSearchIcon />
+      <fieldset>
+        <legend>Filter by category</legend>
+        {categoryDrinks
+          .map((category) => (<CategoryButton
+            key={ category }
+            categoryName={ category }
+          />))}
+      </fieldset>
       {recipesDrinks.map(({ idDrink, strDrink, strDrinkThumb }, index) => (
         <RecipeCard
           key={ idDrink }
