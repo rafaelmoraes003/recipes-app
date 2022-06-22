6d3f48674c22c532c6c84b0e04d2d547803d2f32
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-21 21:25:29 -0300

    feat (actions/index.js): Action saveInitialFoods

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -5,7 +5,16 @@ const saveUser = (user) => ({
   payload: user,
 });
 
+const SAVE_INITIAL_FOODS = 'SAVE_INITIAL_FOODS';
+
+const saveInitialFoods = (recipes) => ({
+  type: SAVE_INITIAL_FOODS,
+  payload: recipes,
+});
+
 export {
   SAVE_USER,
   saveUser,
+  SAVE_INITIAL_FOODS,
+  saveInitialFoods,
 };
