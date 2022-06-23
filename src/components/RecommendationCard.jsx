af887a63a2a8e2009e5c5b2cb554688a2aec8ffe
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-23 00:05:34 -0300

    feat: Criação do RecommedationCard

---
+++ src/components/RecommendationCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function RecommendationCard({
+  id, index, recommendationImage, recommendationName, endPoint }) {
+  console.log(endPoint);
+  return (
+    <div className="recommendationCard">
+      <Link
+        data-testid={ `${index}-recomendation-card` }
+        to={ `../${endPoint}/${id}` }
+      >
+        <button type="button"><h3>{ recommendationName }</h3></button>
+      </Link>
+      <img src={ recommendationImage } alt={ recommendationName } />
+    </div>
+  );
+}
+
+RecommendationCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  recommendationImage: PropTypes.string.isRequired,
+  recommendationName: PropTypes.string.isRequired,
+  endPoint: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+export default RecommendationCard;
