e88da9489fbb163a12f16fcbe03ffb05efe4fe9f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-22 22:25:45 -0300

    feat: Chamada de API e renderização de receita

--- src/pages/DrinkDetail.jsx
+++ src/pages/DrinkDetail.jsx
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { fetchDrinks } from '../helpers/fetchRecipesAPI';
 
-const DrinkDetail = () => (
-  <section>
-    <img src="123" alt="123" data-testid="recipe-photo" />
-    <h1 data-testid="recipe-title">123</h1>
-    <h3 data-testid="recipe-category">123</h3>
-    <button type="button" data-testid="share-btn">Compartilhar Receita</button>
-    <button type="button" data-testid="favorite-btn">Favoritar Receita</button>
-    {/* TODO: ingredients => data-testid="${index}ingredient-name-and-measure"} */}
-    <p data-testid="instructions">123</p>
-    {/* TODO: receitas recomendadas => data-testid="start-recipe-btn" */}
-  </section>
-);
+const DrinkDetail = () => {
+  const history = useHistory();
+  const [recipe, setRecipe] = useState({ ingredientsAndMeasures: [] });
+
+  useEffect(() => {
+    const id = history.location.pathname.split('/')[2];
+    const fetchRecipe = async () => {
+      const recipeData = await fetchDrinks(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      console.log(recipeData);
+      const entries = Object.entries(recipeData[0]);
+      const ingredients = entries
+        .filter((item) => JSON.stringify(item).includes('strIngredient'))
+        .filter((ingredient) => ingredient[1] !== null);
+      const measures = entries
+        .filter((item) => JSON.stringify(item).includes('strMeasure'))
+        .filter((measure) => measure[1] !== null);
+      const ingredientsAndMeasures = ingredients
+        .map((ingredient, index) => `${ingredient[1]} - ${measures[index][1]}`);
+      setRecipe({ ...recipeData[0], ingredientsAndMeasures });
+    };
+    fetchRecipe();
+  }, [history]);
+  return (
+    <section>
+      <img
+        src={ recipe.strDrinkThumb }
+        alt={ recipe.strDrink }
+        data-testid="recipe-photo"
+      />
+      <h1 data-testid="recipe-title">{ recipe.strDrink }</h1>
+      <h3 data-testid="recipe-category">{ recipe.strCategory }</h3>
+      <button type="button" data-testid="share-btn">Compartilhar Receita</button>
+      <button type="button" data-testid="favorite-btn">Favoritar Receita</button>
+      <ul>
+        <h3>Ingredients</h3>
+        {recipe.ingredientsAndMeasures
+          .map((i, index) => (
+            <li
+              key={ i }
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              { i }
+            </li>
+          ))}
+      </ul>
+      <p data-testid="instructions">{ recipe.strInstructions }</p>
+      <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+      {/* TODO: receitas recomendadas => data-testid="start-recipe-btn" */}
+    </section>
+  );
+};
 export default DrinkDetail;
