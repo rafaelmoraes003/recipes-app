62267a3aeff3b9c629fc107780b28462792e37ff
martins <gabrielamspinola@gmail.com>
2022-06-23 18:41:24 -0300

    Req 70 a 73

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const ExploreDrinks = () => (
-  <div>
-    <Header title="Explore Drinks" showSearchIcon={ false } />
-    <Footer />
-  </div>
-);
+const ExploreDrinks = () => {
+  const history = useHistory();
+  return (
+    <div>
+      <Header title="Explore Drinks" showSearchIcon={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => { history.push('./pages/ExploreDrinksIngredients'); } }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => { history.push('/explore/drinks'); } }
+      >
+        Surprise me!
+      </button>
+      <Footer />
+    </div>
+  );
+};
 export default ExploreDrinks;
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const ExploreFoods = () => (
-  <div>
-    <Header title="Explore Foods" showSearchIcon={ false } />
-    <Footer />
-  </div>
+const ExploreFoods = () => {
+  const history = useHistory();
+  return (
+    <div>
+      <Header title="Explore Foods" showSearchIcon={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => { history.push('./pages/ExploreFoodsIngredients'); } }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => { history.push('./pages/ExploreFoodNationality'); } }
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => { history.push('/explore/drinks'); } }
+      >
+        Surprise me!
+      </button>
+      <Footer />
+    </div>
+  );
+};
 
-);
 export default ExploreFoods;
