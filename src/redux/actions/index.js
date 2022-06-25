d6be1aaf3be81910b7b157d70e367c8925adeb7e
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-25 13:44:54 -0300

    feat: inclui action para salvar receitas iniciadas

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,29 +1,34 @@
-const SAVE_USER = 'SAVE_USER';
+export const SAVE_USER = 'SAVE_USER';
 
-const saveUser = (user) => ({
+export const saveUser = (user) => ({
   type: SAVE_USER,
   payload: user,
 });
 
-const SAVE_INITIAL_FOODS = 'SAVE_INITIAL_FOODS';
+export const SAVE_INITIAL_FOODS = 'SAVE_INITIAL_FOODS';
 
-const saveInitialFoods = (recipes) => ({
+export const saveInitialFoods = (recipes) => ({
   type: SAVE_INITIAL_FOODS,
   payload: recipes,
 });
 
-const SAVE_INITIAL_DRINKS = 'SAVE_INITIAL_DRINKS';
+export const SAVE_INITIAL_DRINKS = 'SAVE_INITIAL_DRINKS';
 
-const saveInitialDrinks = (recipes) => ({
+export const saveInitialDrinks = (recipes) => ({
   type: SAVE_INITIAL_DRINKS,
   payload: recipes,
 });
 
-export {
-  SAVE_USER,
-  saveUser,
-  SAVE_INITIAL_FOODS,
-  saveInitialFoods,
-  SAVE_INITIAL_DRINKS,
-  saveInitialDrinks,
-};
+export const SAVE_STARTERED_FOODS = 'SAVE_STARTERED_FOODS';
+
+export const SAVE_STARTERED_DRINKS = 'SAVE_STARTERED_DRINKS';
+
+export const saveStarteredFoods = (recipes) => ({
+  type: SAVE_STARTERED_FOODS,
+  payload: recipes,
+});
+
+export const saveStarteredDrinks = (recipes) => ({
+  type: SAVE_STARTERED_DRINKS,
+  payload: recipes,
+});
