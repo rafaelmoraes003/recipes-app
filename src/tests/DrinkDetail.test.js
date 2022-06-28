5dc6ed1152900860fc9c00eb45ae9575583cccd8
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-28 15:54:22 -0300

    test: 1º it, verifica receita renderizada

--- src/tests/DrinkDetail.test.js
+++ src/tests/DrinkDetail.test.js
@@ -5,7 +5,7 @@ import { act } from 'react-dom/test-utils';
 import renderWithRouterAndRedux from './renderWithRouterAndRedux';
 import FoodDetail from '../pages/FoodDetail';
 import oneMeal from '../../cypress/mocks/oneMeal';
-import drinks from '../../cypress/mocks/drinks';
+import meals from '../../cypress/mocks/meals';
 import oneDrink from '../../cypress/mocks/oneDrink';
 import { foodsInLocalStorage } from '../helpers/storageFuncs';
 import App from '../App';
@@ -36,5 +36,37 @@ describe('Testa o componente DrinkDetail e suas funcionalidades', () => {
   };
   afterEach(() => {
     jest.spyOn(global, 'fetch').mockRestore();
+  });
+  it('Verifica se a página renderiza a receita e suas informações', async () => {
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce(oneDrink),
+    });
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce(meals),
+    });
+    const { history } = renderWithRouterAndRedux(
+      <FoodDetail />,
+      {},
+      pathname,
+    );
+    await wait(() => {
+      expect(history.location.pathname).toBe('/drinks/15997');
+
+      const title = screen.getByTestId('recipe-title');
+      const image = screen.getByTestId('recipe-photo');
+      const category = screen.getByTestId('recipe-category');
+      const ingredient0 = screen.getByTestId('0-ingredient-name-and-measure');
+      const ingredient1 = screen.getByTestId('1-ingredient-name-and-measure');
+      const ingredient2 = screen.getByTestId('2-ingredient-name-and-measure');
+      const instructions = screen.getByTestId('instructions');
+
+      expect(image).toHaveProperty('src', oneDrink.drinks[0].strDrinkThumb);
+      expect(title).toHaveTextContent(oneDrink.drinks[0].strDrink);
+      expect(category).toHaveTextContent(oneDrink.drinks[0].strAlcoholic);
+      expect(ingredient0).toHaveTextContent(/Hpnotiq/i);
+      expect(ingredient1).toHaveTextContent(/Pineapple Juice/i);
+      expect(ingredient2).toHaveTextContent(/Banana Liqueur/i);
+      expect(instructions).toHaveTextContent(oneDrink.drinks[0].strInstructions);
+    });
   });
 });
