a530a5bcfe24ead3f8a6c20e039882b699796488
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-28 17:51:22 -0300

    test: it testa acesso aos cards de recomendações

--- src/tests/DrinkDetail.test.js
+++ src/tests/DrinkDetail.test.js
@@ -7,7 +7,6 @@ import DrinkDetail from '../pages/DrinkDetail';
 import oneMeal from '../../cypress/mocks/oneMeal';
 import meals from '../../cypress/mocks/meals';
 import oneDrink from '../../cypress/mocks/oneDrink';
-import { foodsInLocalStorage } from '../helpers/storageFuncs';
 import App from '../App';
 
 describe('Testa o componente DrinkDetail e suas funcionalidades', () => {
@@ -116,6 +115,59 @@ describe('Testa o componente DrinkDetail e suas funcionalidades', () => {
         expect(navigator.clipboard.writeText).toBeCalled();
         expect(navigator.clipboard.writeText).toBeCalledWith('http://localhost:3000/drinks/178319');
         expect(screen.getByText('Link copied!')).toBeDefined();
+      });
+    });
+  });
+  it('Verifica se é possível acessar food de recomendação', async () => {
+    await act(async () => {
+      jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+        json: jest.fn().mockResolvedValueOnce(oneDrink),
+      });
+      jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+        json: jest.fn().mockResolvedValueOnce(meals),
+      });
+      jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+        json: jest.fn().mockResolvedValueOnce(oneMeal),
+      });
+      const { history } = renderWithRouterAndRedux(
+        <App />,
+        {},
+        pathname,
+      );
+      const recomendationCard = await screen.findByTestId('0-recomendation-card');
+
+      userEvent.click(recomendationCard);
+
+      await wait(() => {
+        expect(history.location.pathname).toBe('/foods/52977');
+
+        const image = screen.getByTestId('recipe-photo');
+        const title = screen.getByTestId('recipe-title');
+        const category = screen.getByTestId('recipe-category');
+        const ingredient0 = screen.getByTestId('0-ingredient-name-and-measure');
+        const ingredient1 = screen.getByTestId('1-ingredient-name-and-measure');
+        const ingredient2 = screen.getByTestId('2-ingredient-name-and-measure');
+        const ingredient3 = screen.getByTestId('3-ingredient-name-and-measure');
+        const ingredient4 = screen.getByTestId('4-ingredient-name-and-measure');
+        const ingredient5 = screen.getByTestId('5-ingredient-name-and-measure');
+        const ingredient6 = screen.getByTestId('6-ingredient-name-and-measure');
+        const ingredient7 = screen.getByTestId('7-ingredient-name-and-measure');
+        const instructions = screen.getByTestId('instructions');
+        const video = screen.getByTestId('video');
+
+        expect(image.src).toBe(oneMeal.meals[0].strMealThumb);
+        expect(title).toHaveTextContent(oneMeal.meals[0].strMeal);
+        expect(category).toHaveTextContent(oneMeal.meals[0].strCategory);
+        expect(ingredient0).toHaveTextContent(/penne rigate - 1 pound/i);
+        expect(ingredient1).toHaveTextContent(/olive oil/i);
+        expect(ingredient2).toHaveTextContent(/garlic - 3 cloves/i);
+        expect(ingredient3).toHaveTextContent(/chopped tomatoes - 1 tin/i);
+        expect(ingredient4).toHaveTextContent(/red chile flakes/i);
+        expect(ingredient5).toHaveTextContent(/italian seasoning/i);
+        expect(ingredient6).toHaveTextContent(/basil - 6 leaves/i);
+        expect(ingredient7).toHaveTextContent(/Parmigiano-Reggiano - spinkling/i);
+        expect(instructions).toHaveTextContent(oneMeal.meals[0].strInstructions);
+        expect(video.src).toBe('https://www.youtube.com/embed/1IszT_guI08');
       });
     });
   });
