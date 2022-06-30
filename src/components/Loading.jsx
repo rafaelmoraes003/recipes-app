06815e5deb9c09080fd37eaf1703b000f0d78ffc
rafael <rafaelsm003@gmail.com>
2022-06-30 00:53:06 -0300

    feat: cria barra de laoding

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style/Loading.css';
+
+const Loading = () => (
+  <div className="loading-container">
+    <div className="loading" />
+  </div>
+);
+
+export default Loading;
