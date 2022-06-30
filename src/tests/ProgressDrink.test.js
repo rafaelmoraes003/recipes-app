50b3293f5d77d3200060bd5862fa15dd250a9129
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-30 16:19:22 -0300

    test: verifica compartilhar e favoritar receita

--- src/tests/ProgressDrink.test.js
+++ src/tests/ProgressDrink.test.js
@@ -97,4 +97,39 @@ describe('Testa o componente ProgressDrink e suas funcionalidades', () => {
       expect(screen.getByTestId('page-title')).toHaveTextContent('Done Recipes');
     });
   });
+  it('Verifica se é possível favoritar e compartilhar link de receita', async () => {
+    await act(async () => {
+      const mockClipboard = {
+        writeText: jest.fn(),
+      };
+      global.navigator.clipboard = mockClipboard;
+      jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+        json: jest.fn().mockResolvedValueOnce(oneDrink),
+      });
+      renderWithRouterAndRedux(
+        <ProgressDrinks />,
+        {},
+        pathname,
+      );
+      const favoriteButton = await screen.findByTestId('favorite-btn');
+      const shareButton = await screen.findByTestId('share-btn');
+
+      expect(favoriteButton).toHaveProperty('src', 'http://localhost/whiteHeartIcon.svg');
+      userEvent.click(favoriteButton);
+      expect(favoriteButton).toHaveProperty('src', 'http://localhost/blackHeartIcon.svg');
+      await wait(() => {
+        expect(localStorage).toHaveProperty('favoriteRecipes');
+      });
+      userEvent.click(favoriteButton);
+      expect(favoriteButton).toHaveProperty('src', 'http://localhost/whiteHeartIcon.svg');
+
+      userEvent.click(shareButton);
+
+      await wait(() => {
+        expect(navigator.clipboard.writeText).toBeCalled();
+        expect(navigator.clipboard.writeText).toBeCalledWith('http://localhost:3000/drinks/178319');
+        expect(screen.getByText('Link copied!')).toBeInTheDocument();
+      });
+    });
+  });
 });
